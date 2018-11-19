f5894fe3f2e7c7c49a748001c52496e38dacf64c
Explorer001 <lukas.gehreke@gmail.com>
2018-11-19 21:45:29 +0100

    added fancy menu bar

--- lib/main.dart
+++ lib/main.dart
@@ -70,6 +70,37 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(child: Text('Festivapp')),
+            ListTile(
+              title: Text('Kalender'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Checkliste'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Wetter'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Bierbörse'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Karte'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Runningorder'),
+              onTap: () {},
+            )
+          ],
+        )
+      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
