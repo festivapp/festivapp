f7753dd077f9eedf2b562f5d724e58d061b9b749
Johann Wagner <wagner@creatdev-solutions.com>
2018-11-20 15:18:36 +0100

    Added Weather.

--- lib/page_weather.dart
+++ lib/page_weather.dart
@@ -2,14 +2,28 @@ import 'package:flutter/material.dart';
 import 'drawer.dart';
 
 class PageWeather extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Wetter'),
       ),
       drawer: FestivappDrawer(),
+      body: Center(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+
+          Text('26 Grad', textScaleFactor: 2, style: textTheme.body2),
+          Text('Sonne', textScaleFactor: 2, style: textTheme.body2),
+          Image.network('https://placehold.it/600x600'),
+          Text('Magdeburg, Deutschland',
+              textScaleFactor: 1.5, style: textTheme.body2),
+        ],
+      )),
     );
   }
 }
