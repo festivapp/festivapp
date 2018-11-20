70cdec9127813f9ec1dedcfee991b667a176e8fe
Explorer001 <lukas.gehreke@gmail.com>
2018-11-20 18:16:57 +0100

    working checklist markers

--- lib/page_checklist.dart
+++ lib/page_checklist.dart
@@ -9,6 +9,7 @@ class PageChecklist extends StatefulWidget {
 class CheckList extends State<PageChecklist> {
 
   List<String> items = [];
+  List<bool> states = [];
   final TextEditingController ctrl = new TextEditingController();
 
   @override
@@ -32,12 +33,13 @@ class CheckList extends State<PageChecklist> {
               child: ListView.builder(
                   itemCount: items.length,
                   itemBuilder: (BuildContext context, int index) {
+                      states.add(false);
                       return CheckboxListTile(
                         title: Text(items[index]),
-                        value: false,
+                        value: states[index],
                         onChanged: (bool val) {
                           setState(() {
-
+                            states[index] = !states[index];
                           });
                         }
                     );
