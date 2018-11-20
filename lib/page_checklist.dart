b0d013e69f7efa654eb990967f567e9bbf0691a2
Explorer001 <lukas.gehreke@gmail.com>
2018-11-20 18:02:20 +0100

    added checklist

--- lib/page_checklist.dart
+++ lib/page_checklist.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'drawer.dart';
 
-class PageChecklist extends StatelessWidget {
+class PageChecklist extends StatefulWidget {
+
+  State createState() => new CheckList();
+}
+
+class CheckList extends State<PageChecklist> {
+
+  List<String> items = [];
+  final TextEditingController ctrl = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +18,35 @@ class PageChecklist extends StatelessWidget {
         title: Text('Checkliste'),
       ),
       drawer: FestivappDrawer(),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            controller: ctrl,
+            onSubmitted: (text) {
+              items.add(text);
+              ctrl.clear();
+              setState(() {});
+            },
+          ),
+          new Expanded(
+              child: ListView.builder(
+                  itemCount: items.length,
+                  itemBuilder: (BuildContext context, int index) {
+                      return CheckboxListTile(
+                        title: Text(items[index]),
+                        value: false,
+                        onChanged: (bool val) {
+                          setState(() {
+
+                          });
+                        }
+                    );
+                  }
+              )
+          ),
+        ],
+      ),
     );
   }
+
 }
